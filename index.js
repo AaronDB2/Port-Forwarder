ab16aec47ca6ca3d1c952d6c186c974063c29108
Aaron de Boer <aarondeboerwk@gmail.com>
2023-03-25 16:04:35 +0100

    Added caching to the web servers

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const NodeCache = require("node-cache");
+
+const cache = new NodeCache();
 
 // Initialize web server 1
 const app1 = express();
@@ -6,18 +9,37 @@ const app1 = express();
 // Initialize web server 2
 const app2 = express();
 
-// Handler method
-const handler = (num) => (req, res) => {
-  // Deconstruct request properties
-  const { method, url, headers, body } = req;
+// Initialize web server 3
+const app3 = express();
 
-  // Send response
-  res.send("Response from server " + num);
+// Handler method that sends responses and caches
+const handler = (num) => (req, res) => {
+  // Make a response
+  const response = `Response from server ${num}`;
+
+  // Check if there is a cached response with url and portnumber as key
+  const cachedResponse = cache.get(req.headers.host);
+
+  // Compare response and cachedResponse
+  if (response !== cachedResponse) {
+    console.log("Did not find cache key");
+    // Set cache with url and port number as key
+    cache.set(req.headers.host, response);
+    res.setHeader("Cache-Control", "public, max-age=3600");
+
+    // Send response as json
+    res.json({ message: response });
+  } else {
+    console.log("Found cache key");
+    // Send cachedResponse as json
+    return res.json({ message: cachedResponse });
+  }
 };
 
 // Handle GET and POST requests
 app1.get("*", handler(1)).post("*", handler(1));
 app2.get("*", handler(2)).post("*", handler(2));
+app3.get("*", handler(3)).post("*", handler(3));
 
 // Start server on PORT 3000
 app1.listen(3000, (err) => {
@@ -31,4 +53,11 @@ app2.listen(3001, (err) => {
   err
     ? console.log("Failed to listen on PORT 3001")
     : console.log("Web Server " + "listening on PORT 3001");
+});
+
+// Start server on PORT 3002
+app3.listen(3002, (err) => {
+  err
+    ? console.log("Failed to listen on PORT 3002")
+    : console.log("Web Server " + "listening on PORT 3002");
 });
--- loadbalancer.js
+++ loadbalancer.js
@@ -1,20 +1,24 @@
 const express = require("express");
-const path = require("path");
-const axios = require("axios");
+const morgan = require("morgan");
 
 // Make app instance
 const app = express();
+
+// Middleware for logging
+app.use(morgan("dev"));
+
 // Web servers for the loadbalancer to use
-const webServers = ["http://localhost:3000", "http://localhost:3001"];
+const webServers = [
+  "http://localhost:3000",
+  "http://localhost:3001",
+  "http://localhost:3002",
+];
+
 // Track current web server for sending requests
 let current = 0;
 
 // Forward to web server
 const handler = async (req, res) => {
-  // Adding headers to request to disable caching
-  req.headers["Cache-Control"] = "no-cache";
-  req.headers["Pragma"] = "no-cache";
-  req.headers["Expires"] = "0";
   // Destruct properties from request body
   const { method, url, headers, body } = req;
 
@@ -27,21 +31,13 @@ const handler = async (req, res) => {
   } else {
     current = 0;
   }
-
-  try {
-    // Requesting to web server
-    const response = await axios({
-      url: `${server}${url}`,
-      method: method,
-      headers: headers,
-      data: body,
+  // Requesting to web server
+  fetch(`${server}${url}`)
+    .then((response) => response.json())
+    .then((data) => res.send(data))
+    .catch((error) => {
+      console.log(error);
     });
-
-    // Send the response from the web server back to the client
-    res.send(response.data);
-  } catch (err) {
-    res.status(500).send(err);
-  }
 };
 
 // Get request for sending favicon
